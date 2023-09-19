62964e9f96bfa108ae937d366f326cd65927b01d
AnuragBisht71 <bishtanurag71@gmail.com>
2023-09-19 19:38:19 +0530

    use effect 3 case

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -2,20 +2,27 @@ import { useEffect, useState } from "react";
 
 let App = () => {
   let [count, setCount] = useState(0);
+  let [process, setProcess] = useState("running");
 
   // useEffect => it is a hook which is used inside a functional component
   // it takes two argument => one: function, two: array [which is optional] based on the passed array or not
   // we have 3 cases :
 
-  // Case 1: Passed a Function and an Empty array => use effect function will calls the passed function only once, after the first render. So it works like COMPONENT DID MOUNT
+  // Case 1: Passed a Function and an Empty array => use effect function will calls the passed function only once, that is after the first render. So it works like COMPONENT DID MOUNT
   useEffect(() => {
     console.log("Case 1 use effect was called");
   }, []);
 
-  // Case 2: Passed a Function and no Array => use effect function will execute the passed function after every render, that is after first render and every re-render.
+  // Case 2: Passed a Function and no Array => use effect function will execute the passed function after first render and every re-render.
   useEffect(() => {
     console.log("Case 2 use effect was called");
   });
+
+  // Case 3: Passed a Function and a state in Array => use effect function will execute the passed function after first render and after every state variable passed in array change.
+  useEffect(() => {
+    let arr = process.split("i");
+    console.log(arr);
+  }, [process]);
 
   return (
     <div>
@@ -33,6 +40,19 @@ let App = () => {
         }}
       >
         -
+      </button>
+
+      <p>{process}</p>
+      <button
+        onClick={() => {
+          if (process == "running") {
+            setProcess("stop");
+          } else {
+            setProcess("running");
+          }
+        }}
+      >
+        kill process
       </button>
     </div>
   );
