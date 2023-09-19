d2a6de52c161ee00b5b095351ab6be83820eadc6
AnuragBisht71 <bishtanurag71@gmail.com>
2023-09-19 18:23:00 +0530

    hooks (useState)

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,5 +1,27 @@
+import { useState } from "react";
+
 let App = () => {
-  return <h1>Hooks</h1>;
+  let [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        +
+      </button>
+      <p>{count}</p>
+      <button
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      >
+        -
+      </button>
+    </div>
+  );
 };
 
 export default App;
