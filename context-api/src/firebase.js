d2610149b4bd996af2accae1c529b69c5281f713
AnuragBisht71 <bishtanurag71@gmail.com>
2023-09-19 21:25:02 +0530

    firebase initialize

---
+++ context-api/src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "./firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDMNzYCiS1qT0ZjZ89SkI82QzBanmUUqZc",
+  authDomain: "test-1-6527d.firebaseapp.com",
+  projectId: "test-1-6527d",
+  storageBucket: "test-1-6527d.appspot.com",
+  messagingSenderId: "311178854068",
+  appId: "1:311178854068:web:2a25d644ed79f27a8421e1",
+};
+
+
+firebase.initalizeApp(firebaseConfig);
+
