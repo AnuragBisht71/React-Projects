b387030215e6db89371a11e3f0fa92e56b5246ac
AnuragBisht71 <bishtanurag71@gmail.com>
2023-09-19 19:08:08 +0530

    use effect 1 case

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,7 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 let App = () => {
   let [count, setCount] = useState(0);
+
+  // useEffect => it is a hook which is used inside a functional component
+  // it takes two argument => one: function, two: array [which is optional] based on the passed array or not
+  // we have 3 cases :
+
+  // 1. Passed a Function and an Empty array => use effect function will calls the passed function only once, after the first render. So it works like COMPONENT DID MOUNT
+  useEffect(() => {
+    console.log("use effect was called");
+  }, []);
 
   return (
     <div>
