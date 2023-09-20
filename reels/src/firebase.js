87a78016f3fc8f45827c6beace81d69cbb69cea6
AnuragBisht71 <bishtanurag71@gmail.com>
2023-09-20 07:37:01 +0530

    reels app setup

---
+++ reels/src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "./firebase/App";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDMNzYCiS1qT0ZjZ89SkI82QzBanmUUqZc",
+  authDomain: "test-1-6527d.firebaseapp.com",
+  projectId: "test-1-6527d",
+  storageBucket: "test-1-6527d.appspot.com",
+  messagingSenderId: "311178854068",
+  appId: "1:311178854068:web:2a25d644ed79f27a8421e1",
+};
+
+firebase.initializeApp(firebaseConfig);
