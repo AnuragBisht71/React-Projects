4faf027503d6d25f30daa01cb4fbdff5cb6dc3f9
AnuragBisht71 <bishtanurag71@gmail.com>
2023-10-03 16:42:31 +0530

    firestore add and get collection

--- reels/src/App.jsx
+++ reels/src/App.jsx
@@ -1,20 +1,37 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
 import Home from "./component/Home";
 import Login from "./component/Login";
 import AuthProvider from "./AuthProvider";
+import { firestore } from "./firebase";
+import { useEffect } from "react";
 
 let App = () => {
+  useEffect(() => {
+    // ADD
+    // firestore.collection("users").add({ body: "this is some value" });
+
+    // GET COMPLETE COLLECTION DATA
+    async function f() {
+      let querySnapshot = await firestore.collection("users").get();
+
+      for (let i = 0; i < querySnapshot.docs.length; i++) {
+        console.log(querySnapshot.docs[i].data());
+      }
+    }
+    f();
+  }, []);
+
   return (
     <>
-      <AuthProvider>
+      <h1 className="m-4">App</h1>
+      {/* <AuthProvider>
         <Router>
           <Routes>
             <Route exact path="/login" element={<Login />}></Route>
             <Route exact path="/" element={<Home />}></Route>
           </Routes>
         </Router>
-      </AuthProvider>
+      </AuthProvider> */}
     </>
   );
 };
--- reels/src/firebase.js
+++ reels/src/firebase.js
@@ -8,6 +8,7 @@ firebase.initializeApp(config);
 let provider = new firebase.auth.GoogleAuthProvider();
 
 export const auth = firebase.auth();
+export const firestore = firebase.firestore();
 
 export const signInWithGoogle = () => {
   auth.signInWithPopup(provider);
