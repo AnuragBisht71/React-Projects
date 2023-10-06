663785ba6fc2ccbe7c6c728e0d56316fbdcdd7bd
AnuragBisht71 <bishtanurag71@gmail.com>
2023-10-06 17:13:14 +0530

    added upload functionality

--- reels/src/component/Home.jsx
+++ reels/src/component/Home.jsx
@@ -1,4 +1,4 @@
-import { auth } from "../firebase";
+import { auth, storage } from "../firebase";
 import { authContext } from "../AuthProvider";
 import { useContext } from "react";
 import { Navigate } from "react-router-dom";
@@ -25,6 +25,30 @@ let Home = () => {
       >
         Logout
       </button>
+
+      <input
+        type="file"
+        onChange={(e) => {
+          let videoObj = e.currentTarget.files[0];
+          let { name, size, type } = videoObj;
+
+          size = size / 1000000;
+
+          if (size > 10) {
+            alert("file size exceeds 10mb");
+            return;
+          }
+
+          type = type.split("/")[0];
+
+          if (type !== "video") {
+            alert("Please upload a video file");
+            return;
+          }
+
+          storage.ref(`/post/${user.uid}/${Date.now() + name}`).put(videoObj);
+        }}
+      />
     </>
   );
 };
--- reels/src/firebase.js
+++ reels/src/firebase.js
@@ -2,6 +2,7 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 import config from "./config.json";
+import "firebase/compat/storage";
 
 firebase.initializeApp(config);
 
@@ -9,6 +10,7 @@ let provider = new firebase.auth.GoogleAuthProvider();
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
+export const storage = firebase.storage();
 
 export const signInWithGoogle = () => {
   auth.signInWithPopup(provider);
