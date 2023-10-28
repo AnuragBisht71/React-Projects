14d3f545ea99b2cfa865c2a009c0def1f2e18422
AnuragBisht71 <bishtanurag71@gmail.com>
2023-10-28 19:50:33 +0530

    redux initialized

--- redux-counter/src/index.js
+++ redux-counter/src/index.js
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import reducer from "./redux/reducer";
+
+import { configureStore } from "redux";
+import { Provider } from "react-redux";
+
+let myStore = configureStore(reducer);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <Provider store={myStore}>
+    <App />
+  </Provider>
+);
